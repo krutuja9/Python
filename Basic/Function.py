1eb9586ce235cb6f5dae1f60bbd31e7654374798
krutuja9 <krutuja609@gmail.com>
2024-02-07 19:58:48 +0530

    Function basic

---
+++ Basic/Function.py
@@ -0,0 +1,13 @@
+def greetHello(name,greet):
+  print("hello" ,name, greet)
+  print("hello rutuja")
+
+# print("executing function...")
+# greetHello("rutuja", "how are you")
+# greetHello("rutujakokate","hey there")
+  
+def letterGenerator(name, date):
+  st = f"hi mam \nThis is {name} I will not come to school on {date}"
+  print(st)
+
+letterGenerator("rutuja","10th feb 2024")
