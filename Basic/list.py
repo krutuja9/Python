2f6cbe4af0f38cf5824bda9e9621d63bfa74f0e5
krutuja9 <krutuja609@gmail.com>
2024-02-07 13:25:03 +0530

    List and UserInput

---
+++ Basic/list.py
@@ -0,0 +1,15 @@
+l1 = [99993,4,5,6,3,3,3]
+
+print(type(l1))
+print(l1)
+# l1.remove("rutuja")
+# print(l1.count(3))
+# l1.sort()
+# print(l1)
+# print(l1.pop())
+# l1.append(76)
+# l1.copy()
+l1.extend([89,12,23,00])
+print(l1)
+
+
---
+++ Basic/userInput.py
@@ -0,0 +1,11 @@
+name = input("enter your name:")
+print(name)
+
+# write a python program to tae a number from user  as an input
+num = input("enter a number:")
+# print(num)
+print(type(num))
+print (int(num) + 1)
+
+a1 = '''hello'''
+print(a1)
